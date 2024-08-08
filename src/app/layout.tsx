7b792e939e73160c386e43bbfdd30d6224762e7e
satwikWiz <satwik@wizcommerce.com>
2024-08-08 13:56:56 +0530

    feat: new ui added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/multiple-themes-provider";
+import { title } from "process";
 const DM_Sans_desructured = DM_Sans({ subsets: ["latin"] });
 
 export default function RootLayout({
@@ -11,6 +12,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+       <title>SaaSy - Saas Automation Platform</title>
+       <link rel="icon" href="/fuzzieLogo.png" />
       <body className={DM_Sans_desructured.className}>
           <ThemeProvider
             attribute="class"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,188 @@
+import { CardContainer } from "@/animations/3d-card";
+import { Parallax } from "@/animations/connect-parallax";
 import { ContainerScroll } from "@/animations/controller-scroll-animations";
+import { InfiniteMovingCards } from "@/animations/infinite-moving-cards";
+import { LampContained } from "@/animations/lamp-controlling";
 import NavbarWrappedModal from "@/components/Global/Navbar/NavbarContainers/NavbarWrappedModal";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { FaCheck } from "react-icons/fa6";
+import { CardBody } from "@/animations/3d-card";
+import { CardItem } from "@/animations/3d-card";
+import { clients, products } from "@/lib/constants";
+
 
 export default function Home() {
   return (
-    <main className="relative min-h-screen">
+    <main className="relative">
       <NavbarWrappedModal />
-      <section className="h-screen w-full bg-neutral-950 rounded-md relative flex flex-col items-center antialiased overflow-hidden">
-        <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#698474_150%)]"></div>
+      <section className="w-full bg-neutral-100 rounded-md relative flex flex-col items-center antialiased overflow-hidden">
+        <div className="absolute inset-0  w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
         <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
           <ContainerScroll
             titleComponent={
               <div className="flex items-center flex-col">
-                <h1 className="text-5xl md:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
+                <h1 className="text-5xl md:text-8xl bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
                   Automate Your Work With Fuzzie
                 </h1>
               </div>
             }
           />
+        </div>
+      </section>
+  
+      <InfiniteMovingCards
+        items={clients}
+        className=" w-full"
+        pauseOnHover
+        direction="right"
+        speed="slow"
+      />
+      <section>
+        <Parallax products={products}></Parallax>
+      </section>
+      <section className="mt-[-500px]">
+        <LampContained/>
+        <div className="flex flex-wrap items-center justify-center flex-col md:flex-row gap-8 -mt-72">
+        <CardContainer className="inter-var ">
+            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-neutral-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-full md:!w-[350px] h-auto rounded-xl p-6 border">
+              <CardItem
+                translateZ="50"
+                className="text-xl font-bold text-neutral-600 dark:text-white "
+              >
+                Hobby
+                <h2 className="text-6xl ">$0</h2>
+              </CardItem>
+              <CardItem
+                translateZ="60"
+                className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+              >
+                Get a glimpse of what our software is capable of. Just a heads
+                up {"you'll"} never leave us after this!
+                <ul className="my-4 flex flex-col gap-2">
+                  <li className="flex items-center gap-2">
+                    <FaCheck />3 Free automations
+                  </li>
+                  <li className="flex items-center gap-2">
+                    <FaCheck />
+                    100 tasks per month
+                  </li>
+                  <li className="flex items-center gap-2">
+                    <FaCheck />
+                    Two-step Actions
+                  </li>
+                </ul>
+              </CardItem>
+              <div className="flex justify-between items-center mt-8">
+                <CardItem
+                  translateZ={20}
+                  as="button"
+                  className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                >
+                  Try now →
+                </CardItem>
+                <CardItem
+                  translateZ={20}
+                  as="button"
+                  className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+                >
+                  Get Started Now
+                </CardItem>
+              </div>
+            </CardBody>
+          </CardContainer>
+          <CardContainer className="inter-var ">
+            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-neutral-500/[0.1] dark:bg-black dark:border-[#E2CBFF] border-black/[0.1] w-full md:!w-[350px] h-auto rounded-xl p-6 border">
+              <CardItem
+                translateZ="50"
+                className="text-xl font-bold text-neutral-600 dark:text-white "
+              >
+                Pro Plan
+                <h2 className="text-6xl ">$29</h2>
+              </CardItem>
+              <CardItem
+                translateZ="60"
+                className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+              >
+                Get a glimpse of what our software is capable of. Just a heads
+                up {"you'll"} never leave us after this!
+                <ul className="my-4 flex flex-col gap-2">
+                  <li className="flex items-center gap-2">
+                    <FaCheck />3 Free automations
+                  </li>
+                  <li className="flex items-center gap-2">
+                    <FaCheck />
+                    100 tasks per month
+                  </li>
+                  <li className="flex items-center gap-2">
+                    <FaCheck />
+                    Two-step Actions
+                  </li>
+                </ul>
+              </CardItem>
+              <div className="flex justify-between items-center mt-8">
+                <CardItem
+                  translateZ={20}
+                  as="button"
+                  className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                >
+                  Try now →
+                </CardItem>
+                <CardItem
+                  translateZ={20}
+                  as="button"
+                  className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+                >
+                  Get Started Now
+                </CardItem>
+              </div>
+            </CardBody>
+          </CardContainer>
+          <CardContainer className="inter-var ">
+            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-neutral-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-full md:!w-[350px] h-auto rounded-xl p-6 border">
+              <CardItem
+                translateZ="50"
+                className="text-xl font-bold text-neutral-600 dark:text-white "
+              >
+                Unlimited
+                <h2 className="text-6xl ">$99</h2>
+              </CardItem>
+              <CardItem
+                translateZ="60"
+                className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+              >
+                Get a glimpse of what our software is capable of. Just a heads
+                up {"you'll"} never leave us after this!
+                <ul className="my-4 flex flex-col gap-2">
+                  <li className="flex items-center gap-2">
+                    <FaCheck />3 Free automations
+                  </li>
+                  <li className="flex items-center gap-2">
+                    <FaCheck />
+                    100 tasks per month
+                  </li>
+                  <li className="flex items-center gap-2">
+                    <FaCheck />
+                    Two-step Actions
+                  </li>
+                </ul>
+              </CardItem>
+              <div className="flex justify-between items-center mt-8">
+                <CardItem
+                  translateZ={20}
+                  as="button"
+                  className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                >
+                  Try now →
+                </CardItem>
+                <CardItem
+                  translateZ={20}
+                  as="button"
+                  className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+                >
+                  Get Started Now
+                </CardItem>
+              </div>
+            </CardBody>
+          </CardContainer>
         </div>
       </section>
     </main>
