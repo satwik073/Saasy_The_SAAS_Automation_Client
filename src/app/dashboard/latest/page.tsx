214a6209180c14f7bddc65b2e28e2a605fa80852
satwik073 <satwikkanhere2003@gmail.com>
2024-08-15 22:00:40 +0530

    feat :Auth added

--- src/app/dashboard/latest/page.tsx
+++ src/app/dashboard/latest/page.tsx
@@ -1,9 +1,10 @@
 
+import UserSignIn from '@/app/Auth/UserSignIn';
 import React from 'react';
 export default function Latest(){
     return (
         <main>
-            <h2>Latest</h2>
+            <UserSignIn/>
         </main>
     )
 }
