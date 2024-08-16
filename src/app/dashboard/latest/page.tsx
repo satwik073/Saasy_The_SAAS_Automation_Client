c6e7b7e0123c34544423eb45ae715651b742586b
satwikWiz <satwik@wizcommerce.com>
2024-08-16 13:30:17 +0530

    feat: Grid base created

--- src/app/dashboard/latest/page.tsx
+++ src/app/dashboard/latest/page.tsx
@@ -1,10 +1,10 @@
 
-import UserSignIn from '@/app/Auth/UserSignIn';
+
 import React from 'react';
 export default function Latest(){
     return (
         <main>
-            <UserSignIn/>
+        
         </main>
     )
 }
