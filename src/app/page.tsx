4efb3b2045bec06d334685ad7cb17376504e3dd4
satwik073 <satwikkanhere2003@gmail.com>
2024-08-18 20:00:44 +0530

    fix: eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,38 @@
-import { clients, products } from "@/lib/constants";
+import { clients } from "@/lib/constants";
 import { MAIN_COMPONENT_STORAGE } from ".";
-import React, { ReactNode } from "react";
+import React from "react";
 import { Box } from "@mui/material";
 import { BOX_COMPONENTS_SEPERATED } from "@/constants/variants_data";
-import { DirectionDeterminingConstants, INFINITE_MOVING_CARDS_ATTRIBUTES, SpeedDeterminingConstants } from "@/Pages/InfiniteScrollers/Constants";
+
 interface InfiniteMovingCardsProps {
-  items:{ href: string; }[]; 
+  items: { href: string; }[]; 
   className?: string;
   pauseOnHover?: boolean;
-  direction?: DirectionDeterminingConstants | string
-  speed?: SpeedDeterminingConstants | string
+  direction?: "left" | "right";
+  speed?: "slow";
 }
- const infinite_cards_props : InfiniteMovingCardsProps = {
-  items : clients,
-  className :"w-full",
-  pauseOnHover : true,
-  direction : INFINITE_MOVING_CARDS_ATTRIBUTES.direction_defined_forth,
-  speed : INFINITE_MOVING_CARDS_ATTRIBUTES.speed_determining_constant_low
-}
+
+const infinite_cards_props: InfiniteMovingCardsProps = {
+  items: clients,
+  className: "w-full",
+  pauseOnHover: true,
+  direction: 'right', 
+  speed: 'slow'  
+};
+
 export default function Home() {
   return (
     <React.Fragment>
       <Box component={BOX_COMPONENTS_SEPERATED.components_fetched.main} className="relative">
-      <MAIN_COMPONENT_STORAGE.navbar_junction />
-      <MAIN_COMPONENT_STORAGE.spotlight_preview />
-      <MAIN_COMPONENT_STORAGE.infinite_cards {...infinite_cards_props} /> 
-      <MAIN_COMPONENT_STORAGE.features_section_demo />
-      <MAIN_COMPONENT_STORAGE.featured_section_demo/>
-      <MAIN_COMPONENT_STORAGE.card_containers/>
-      <MAIN_COMPONENT_STORAGE.people_engaged/>
-      <MAIN_COMPONENT_STORAGE.footer/>
-    </Box>
+        <MAIN_COMPONENT_STORAGE.navbar_junction />
+        <MAIN_COMPONENT_STORAGE.spotlight_preview />
+        <MAIN_COMPONENT_STORAGE.infinite_cards {...infinite_cards_props} /> 
+        <MAIN_COMPONENT_STORAGE.features_section_demo />
+        <MAIN_COMPONENT_STORAGE.featured_section_demo />
+        <MAIN_COMPONENT_STORAGE.card_containers />
+        <MAIN_COMPONENT_STORAGE.people_engaged />
+        <MAIN_COMPONENT_STORAGE.footer />
+      </Box>
     </React.Fragment>
   );
 }
