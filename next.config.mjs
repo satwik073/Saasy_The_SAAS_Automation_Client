3cffdac021455f4667349b727e7d9c85f9af0532
satwikWiz <satwik@wizcommerce.com>
2024-09-18 12:11:24 +0530

    fix: next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,26 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 /** @type {import('next').NextConfig} */
+
+// Simulate __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
-    reactStrictMode:true,
-    images: {
-      domains: ['images.unsplash.com', 'assets.aceternity.com'],
-    },
+  reactStrictMode: true,
+
+  images: {
+    domains: ['images.unsplash.com', 'assets.aceternity.com'],
+  },
+
+  webpack: (config) => {
+    // Alias to resolve paths starting with @/
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+
+    // Return the modified config
+    return config;
+  },
 };
 
 export default nextConfig;
